e807f69486f802d7f75c5a3ee50a7b97def238d0
Administrator <2898773126@qq.com>
2024-06-05 23:31:41 +0800

    free

--- config/base_config.py
+++ config/base_config.py
@@ -1,7 +1,7 @@
 # 基础配置
-PLATFORM = "xhs"
-KEYWORDS = "python,golang"
-LOGIN_TYPE = "qrcode"  # qrcode or phone or cookie
+PLATFORM = "ks"
+KEYWORDS = "历史文化"
+LOGIN_TYPE = "phone"  # qrcode or phone or cookie
 COOKIES = ""
 SORT_TYPE = "popularity_descending"  # 具体值参见media_platform.xxx.field下的枚举值，展示只支持小红书
 CRAWLER_TYPE = "search"  # 爬取类型，search(关键词搜索) | detail(帖子详情)| creator(创作者主页数据)
@@ -10,6 +10,7 @@ CRAWLER_TYPE = "search"  # 爬取类型，search(关键词搜索) | detail(帖�
 ENABLE_IP_PROXY = False
 
 # 代理IP池数量
+
 IP_PROXY_POOL_COUNT = 2
 
 # 代理IP提供商名称
@@ -25,7 +26,7 @@ HEADLESS = False
 SAVE_LOGIN_STATE = True
 
 # 数据保存类型选项配置,支持三种类型：csv、db、json
-SAVE_DATA_OPTION = "json"  # csv or db or json
+SAVE_DATA_OPTION = "csv"  # csv or db or json
 
 # 用户浏览器缓存的浏览器文件配置
 USER_DATA_DIR = "%s_user_data_dir"  # %s will be replaced by platform name
@@ -34,7 +35,7 @@ USER_DATA_DIR = "%s_user_data_dir"  # %s will be replaced by platform name
 START_PAGE = 1
 
 # 爬取视频/帖子的数量控制
-CRAWLER_MAX_NOTES_COUNT = 20
+CRAWLER_MAX_NOTES_COUNT = 10000
 
 # 并发爬虫数量控制
 MAX_CONCURRENCY_NUM = 4
